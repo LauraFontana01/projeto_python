3067ba1b1754bdadbb03f62669fc40f5b4ad46c5
LauraFontana01 <fontanameiolaura@hotmail.com>
2024-11-26 16:24:07 -0300

    aula2611

--- aula11_estrudedados.py/atividade_006/atividade_e.py
+++ aula11_estrudedados.py/atividade_006/atividade_e.py
@@ -6,3 +6,19 @@
 # Atividade de Estrutura de Dados
 
 # E) Faça um programa que leia as vogais, depois mostre-as em ordem inversa.
+
+import os
+
+
+os.system('cls')
+
+print('-'*50)
+print('Inversão de vogais!')
+print('-'*50)
+
+lista = ['a', 'e', 'i', 'o', 'u']
+
+vogais = lista[::-1]
+
+print(f'Lista de vogais: {lista}')
+print(f'Lista de vogais invertida: {vogais}')
