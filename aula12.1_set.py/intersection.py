c7c25a427a36f10cd29202f2f05e0f35c4ccdbdf
LauraFontana01 <fontanameiolaura@hotmail.com>
2024-12-02 14:27:21 -0300

    aula0212

---
+++ aula12.1_set.py/intersection.py
@@ -0,0 +1,16 @@
+# Curso Técnico em Desenvovimentos em Sistemas 
+# Turma 0392
+# Autor: Laura Fontana
+# Professor: Sebastião Marcos
+# Data: 02/12/2024
+# Métodos para SET {}: intersection(set2) ou &: Retorna um novo set
+# que é a interseção de dois sets.
+
+import os
+
+
+os.system('cls')
+
+#Título
+print('-'*50)
+print('Testando o método Intersection no set! c:\n')
