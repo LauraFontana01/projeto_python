6ae3ca2222835cb2f331c95892db8b467c8e891d
LauraFontana01 <fontanameiolaura@hotmail.com>
2025-01-13 16:30:09 -0300

    aula13

---
+++ aula13_dicionario/dictpag94.py
@@ -0,0 +1,53 @@
+# Curso Técnico em Desenvovimentos em Sistemas 
+# Turma 0392
+# Autor: Laura Fontana
+# Professor: Sebastião Marcos
+# Data: 13/01/2025
+
+import os
+
+
+os.system('cls')
+
+print('-'*50)
+print('Estrutura de Dados: Dicionário') # dict => {}
+print('-'*50)
+
+compras = {}
+pessoas = {}
+cores = {}
+elementos = dict()
+numeros = dict()
+
+# Atribuindo Valores
+compras['id'] = 1
+compras['item']= 'Caderno'
+compras['valor'] = 10.80
+
+pessoas['id'] = '0010'
+pessoas['nome'] = 'Sherlock Holmes'
+pessoas['endereco'] = 'Baker Street'
+pessoas['numero'] = '2218'
+pessoas['cidade'] = 'Londres'
+pessoas['pais'] =  'Inglaterra'
+
+cores['red'] = 'Vermelho'
+cores['green'] = 'Verde'
+cores['blue'] = 'Azul'
+
+elementos['Pb'] = 'Chumbo'
+elementos['Au'] = 'Ouro'
+elementos['N'] = 'Nitrogênio'
+
+numeros[1] = 100
+numeros[2] = 200
+numeros[3] = 300
+
+# Saída Simples
+print(f'Minhas compras: {compras}')
+print(f'Detetives: {pessoas}')
+print(f'Cor RGB: {cores}')
+print(f'Tabela Períodica: {elementos}')
+print(f'Listagem de números: {numeros}')
+print()
+print('-'*50)
